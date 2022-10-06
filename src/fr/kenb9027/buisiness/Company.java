0d0ec1f2c461a108eef4d19acc9cc3f9312adabc
kenb9027 <kenb9027@gmail.com>
2022-10-06 10:46:08 +0200

    ok for buisiness.classes

--- src/fr/kenb9027/buisiness/Company.java
+++ src/fr/kenb9027/buisiness/Company.java
@@ -8,4 +8,39 @@ public class Company {
     private String name;
 
     private ArrayList<Fly> flies ;
+
+    public Company() {
+        super();
+        this.flies = new ArrayList<Fly>();
+    }
+
+    public Company(String name) {
+        this();
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Company " + name ;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Fly> getFlies() {
+        return flies;
+    }
+
+    public void setFlies(ArrayList<Fly> flies) {
+        this.flies = flies;
+    }
 }
--- src/fr/kenb9027/buisiness/Fly.java
+++ src/fr/kenb9027/buisiness/Fly.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class Fly {
 
-    private static long counter = 0L;
+    private long counter = 0L;
     private long number;
     private float price ;
     private Company company;
@@ -14,5 +14,90 @@ public class Fly {
     private Airport airportDeparture;
     private Airport airportArrival;
 
+    public Fly(long counter) {
+        super();
+        this.number = counter;
+        ++counter;
+    }
 
+    public Fly(
+            float price,
+            Company company,
+            LocalDateTime hourDeparture,
+            LocalDateTime hourArrival,
+            Airport airportDeparture,
+            Airport airportArrival) {
+        //this();
+        this.price = price;
+        this.company = company;
+        this.hourDeparture = hourDeparture;
+        this.hourArrival = hourArrival;
+        this.airportDeparture = airportDeparture;
+        this.airportArrival = airportArrival;
+    }
+
+    @Override
+    public String toString() {
+        return "Fly{" +
+                "number=" + number +
+                ", price=" + price +
+                ", company=" + company +
+                ", hour Departure=" + hourDeparture +
+                ", hour Arrival=" + hourArrival +
+                ", airport Departure=" + airportDeparture +
+                ", airport Arrival=" + airportArrival +
+                '}';
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public LocalDateTime getHourDeparture() {
+        return hourDeparture;
+    }
+
+    public void setHourDeparture(LocalDateTime hourDeparture) {
+        this.hourDeparture = hourDeparture;
+    }
+
+    public LocalDateTime getHourArrival() {
+        return hourArrival;
+    }
+
+    public void setHourArrival(LocalDateTime hourArrival) {
+        this.hourArrival = hourArrival;
+    }
+
+    public Airport getAirportDeparture() {
+        return airportDeparture;
+    }
+
+    public void setAirportDeparture(Airport airportDeparture) {
+        this.airportDeparture = airportDeparture;
+    }
+
+    public Airport getAirportArrival() {
+        return airportArrival;
+    }
+
+    public void setAirportArrival(Airport airportArrival) {
+        this.airportArrival = airportArrival;
+    }
 }
